f8e58b77d811cfe863fe98e7442928a358c92339
Endel Dreyer <endel.dreyer@gmail.com>
2014-08-12 16:14:53 -0300

    example using form post method

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -5,6 +5,10 @@ class HomeController extends Hook\Platform\Controller {
     public function __construct() { }
 
     public function index() {
+        if (Request::isPost()) {
+            var_dump(Input::get());
+        }
+
         $wats = Hook\Model\App::collection('wats')->all();
         $this->view('index', array(
             'item' => $wats[0],
